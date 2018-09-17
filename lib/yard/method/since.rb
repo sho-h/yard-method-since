8a68a2800ca6984980f941b820332abacd33c3e5
Sho Hashimoto <sho.hsmt@gmail.com>
2018-09-17 14:52:17 +0900

    Update output.

--- lib/yard/method/since.rb
+++ lib/yard/method/since.rb
@@ -5,41 +5,34 @@ require 'rugged'
 module Yard
   module Method
     module Since
-      def generate_databases(dir, rc: false, stable: false)
+      def generate_databases(dir, rc: false, stable: false, from: nil, to: nil)
         d = '.' if dir.nil?
-        versions = []
-        repo = Rugged::Repository.new(d)
         Dir.chdir(d) do
-          repo.tags.map(&:name).sort.each do |version|
+          versions(from, to, d).each do |version|
             next if !rc && /-rc1\z/i.match(version)
             next if !stable && /-stable\z/i.match(version)
 
             puts "generating #{version}..."
-            repo.checkout(version, strategy: :force)
+            Rugged::Repository.new(d).checkout(version,
+                                               strategy: :force)
             system('yard', 'doc', '--no-output')
             system('mv', '.yardoc', yardoc_path(version))
-            versions << version
           end
-          return versions
         end
       ensure
         # TODO: back to current.
-        repo = Rugged::Repository.new(d)
-        repo.checkout('master', strategy: :force)
+        Rugged::Repository.new(d).checkout('master',
+                                           strategy: :force)
       end
 
-      def compare_databases(dir, versions)
+      def compare_databases(dir, from: nil, to: nil, github: nil)
         d = '.' if dir.nil?
         res = []
-        regexp = /\A(Added|Modified|Deleted) objects:/
-        type2index = {
-          'added'    => 2,
-          'modified' => 3,
-          'deleted'  => 4,
-        }
+        regexp = /\A(Added|Modified|Removed) objects:/
 
         Dir.chdir(d) do
-          versions.sort.each_cons(2) do |v1, v2|
+          versions(from, to, d).sort.each_cons(2) do |v1, v2|
+            puts "comparing #{v1} and #{v2}..."
             out, st = Open3.capture2('yard', 'diff',
                                   yardoc_path(v1), yardoc_path(v2))
             out.each_line.reject {|s| s == "\n" }.slice_before {|s1, s2|
@@ -48,9 +41,11 @@ module Yard
               t = t.slice(regexp, 1)
               methods.each { |s|
                 m = s.slice(/\A\s*([^\s]+)/, 1)
-                a = [nil, m, '-', '-', '-', '', nil]
-                index = type2index[t.downcase]
-                a[index] = v2
+                a = [nil, m, v2, t.downcase, '', nil]
+                if github
+                  url = github_compare_url(github, v1, v2)
+                  a[4] = "[compare url](#{url})"
+                end
                 res << a
               }
             }
@@ -58,17 +53,40 @@ module Yard
         end
 
         puts <<EOS
-| method | added | modified | deleted | detail |
-|--------|-------|----------|---------|--------|
+| method | version | action | detail |
+|--------|---------|--------|--------|
 EOS
 
           puts res.sort { |a, b|
-            a[1] <=> b[1]
+            # Compare version if method is the same.
+            a[1] == b[1] ? a[2] <=> b[2] : a[1] <=> b[1]
           }.map {|a| a.join('|')}.join("\n")
       end
 
       def yardoc_path(v, d = '.')
         File.join(d, ".yardoc.#{v}")
+      end
+
+      # only github
+      def github_compare_url(github, v1, v2)
+        return File.join('https://github.com', github,
+                         'compare', "#{v1}...#{v2}")
+      end
+
+      def versions(from = nil, to = nil, d = '.')
+        repo = Rugged::Repository.new(d)
+        from_ver = Gem::Version.new(from) if from
+        to_ver = Gem::Version.new(to) if to
+        return repo.tags.map(&:name).select { |v|
+          case
+          when from_ver && Gem::Version.new(v) < from_ver
+            false
+          when to_ver && Gem::Version.new(v) > to_ver
+            false
+          else
+            true
+          end
+        }.sort
       end
     end
   end
